e9a87211300333187703bd12195a0581874e7e02
Marc Breitung <info@marcbreitung.de>
2019-05-31 20:18:06 +0200

    Refactored CanalBuild class

--- src/game/CanalBuilder.ts
+++ src/game/CanalBuilder.ts
@@ -12,6 +12,10 @@ import {
     StandardMaterial
 } from "@babylonjs/core";
 
+import {Button, AdvancedDynamicTexture} from "@babylonjs/gui";
+
+import {Tools} from "./Tools";
+
 import "@babylonjs/core/Meshes/meshBuilder";
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
@@ -25,6 +29,10 @@ export class CanalBuilder {
     private plane: Mesh;
     private camera: ArcRotateCamera;
 
+    private tileRandomPrototypes: Array<Mesh>;
+    private tilePrototypes: Array<Mesh>;
+    private tiles: Array<any>;
+
     private assetsManager: AssetsManager;
     private shadowGenerator: ShadowGenerator;
 
@@ -37,7 +45,7 @@ export class CanalBuilder {
 
         this.scene = new Scene(this.engine);
         this.scene.clearColor = new Color4(0.3, 0.35, 0.4, 1.0);
-        //this.scene.debugLayer.show();
+        this.scene.debugLayer.show();
 
         this.assetsManager = new AssetsManager(this.scene);
         this.plane = null;
@@ -45,48 +53,30 @@ export class CanalBuilder {
         this.currentMesh = null;
         this.startingPoint = null;
 
+        this.tilePrototypes = [];
+        this.tileRandomPrototypes = [];
+
+        this.tiles = [];
+
         this.canvas.addEventListener("pointerdown", (event: PointerEvent) => this.onPointerDown(event), false);
         this.canvas.addEventListener("pointerup", (event: PointerEvent) => this.onPointerUp(event), false);
         this.canvas.addEventListener("pointermove", (event: PointerEvent) => this.onPointerMove(event), false);
     }
 
     public buildScene() {
-        let spotLight = new SpotLight("spotLight", new Vector3(-5, 5, -20), new Vector3(0, -0.25, 1), Math.PI / 2, 2, this.scene);
-        spotLight.intensity = 0.5;
-
-        let ambientLight = new HemisphericLight("ambientLight", new Vector3(0, 0, -10), this.scene);
-        ambientLight.intensity = 0.5;
-
-        this.shadowGenerator = new ShadowGenerator(1024, spotLight);
-        this.shadowGenerator.useBlurExponentialShadowMap = true;
-
-        this.camera = new ArcRotateCamera("camera", Math.PI * 1.5, 2, 25, new Vector3(0, -2, 0), this.scene);
-        this.camera.lowerAlphaLimit = Math.PI * 1.5;
-        this.camera.upperAlphaLimit = Math.PI * 1.5;
-        this.camera.lowerBetaLimit = 2;
-        this.camera.upperBetaLimit = 2;
-        this.camera.lowerRadiusLimit = 25;
-        this.camera.upperRadiusLimit = 25;
-        this.camera.attachControl(this.canvas, true);
-
-        this.plane = Mesh.CreatePlane("plane", 20, this.scene);
-        this.plane.position = Vector3.Zero();
-        this.plane.receiveShadows = true;
-        this.plane.material = this.getMaterial();
-
+        this.addCamera();
         this.loadMesh();
     }
 
-    public loadMesh() {
-        let meshTask = this.assetsManager.addMeshTask("meshTask", "", "/assets/", "tile_a/tile_a.babylon");
-        meshTask.onSuccess = (task) => {
-            let ground = task.loadedMeshes.find((mesh) => mesh.name === 'Ground');
-            ground.parent = this.plane;
-            ground.position = new Vector3(0, 0, 0);
-            this.shadowGenerator.getShadowMap().renderList.push(ground);
-        };
+    public addScene() {
+        this.addPlane();
+        this.addButton();
+        this.addLights();
+        this.addStartGoal();
+    }
 
-        this.assetsManager.load();
+    public resize() {
+        this.engine.resize();
     }
 
     public runRenderLoop() {
@@ -95,16 +85,122 @@ export class CanalBuilder {
         });
     }
 
-    public resize() {
-        this.engine.resize();
+    private loadMesh() {
+        let meshTask = this.assetsManager.addMeshTask("meshTask", "", "/assets/", "tiles.babylon");
+
+        this.tilePrototypes = [];
+        this.tileRandomPrototypes = [];
+
+        meshTask.onSuccess = (task) => {
+            task.loadedMeshes.forEach((mesh) => {
+                if (mesh.name === "Tile D") {
+                    mesh.position = new Vector3(-9, 9, 0);
+                    mesh.rotation = new Vector3(Math.PI / -2, 0, 0);
+                    mesh.isVisible = false;
+                } else {
+                    mesh.position = new Vector3(0, 0, 0);
+                    mesh.rotation = new Vector3(Math.PI / -2, 0, 0);
+                    mesh.isVisible = false;
+                    this.tileRandomPrototypes.push(<Mesh>mesh);
+                }
+                this.tilePrototypes.push(<Mesh>mesh);
+            });
+
+            this.addScene();
+        };
+        this.assetsManager.load();
     }
 
-    public getMaterial() {
+    private addCamera() {
+        this.camera = new ArcRotateCamera("camera", Math.PI * 1.5, 2, 25, new Vector3(0, -2, 0), this.scene);
+        this.camera.lowerAlphaLimit = Math.PI * 1.5;
+        this.camera.upperAlphaLimit = Math.PI * 1.5;
+        this.camera.lowerBetaLimit = 2;
+        this.camera.upperBetaLimit = 2;
+        this.camera.lowerRadiusLimit = 25;
+        this.camera.upperRadiusLimit = 25;
+        this.camera.attachControl(this.canvas, true);
+    }
+
+    private addLights() {
+        let spotLight = new SpotLight("spotLight", new Vector3(-5, 5, -20), new Vector3(0, -0.25, 1), Math.PI / 2, 2, this.scene);
+        spotLight.intensity = 0.5;
+
+        let ambientLight = new HemisphericLight("ambientLight", new Vector3(0, 0, -10), this.scene);
+        ambientLight.intensity = 0.5;
+
+        this.shadowGenerator = new ShadowGenerator(1024, spotLight);
+        this.shadowGenerator.useBlurExponentialShadowMap = true;
+    }
+
+    private addButton() {
+        let advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        let addButton = Button.CreateSimpleButton("addButton", "Add new tile");
+        addButton.width = "150px";
+        addButton.height = "40px";
+        addButton.color = "white";
+        addButton.cornerRadius = 0;
+        addButton.background = "black";
+        addButton.horizontalAlignment = 0;
+        addButton.verticalAlignment = 0;
+        addButton.onPointerUpObservable.add(() => this.getRandomTile());
+        advancedTexture.addControl(addButton);
+    }
+
+    private addPlane() {
+        this.plane = Mesh.CreatePlane("plane", 20, this.scene);
+        this.plane.position = Vector3.Zero();
+        this.plane.receiveShadows = true;
+        this.plane.material = this.getMaterial();
+    }
+
+    private addStartGoal() {
+        let tile_d = this.tilePrototypes.find(e => e.name === 'Tile D');
+
+        let start = tile_d.createInstance("tile_start");
+        start.parent = this.plane;
+        start.position = new Vector3(-9, 9, 0);
+        this.shadowGenerator.getShadowMap().renderList.push(start);
+        this.tiles.push(start);
+
+        let goal = tile_d.createInstance("tile_goal");
+        goal.parent = this.plane;
+        goal.position = new Vector3(9, -9, 0);
+        goal.rotation = new Vector3(Math.PI / 2, 0, Math.PI);
+        this.shadowGenerator.getShadowMap().renderList.push(goal);
+        this.tiles.push(goal);
+    }
+
+    private getRandomTile() {
+        let tile = this.tileRandomPrototypes[Tools.getRandomIntInclusive(0, 2)].createInstance("tile_" + this.tiles.length);
+        tile.parent = this.plane;
+        tile.position = new Vector3(0, 0, -0.5);
+        this.shadowGenerator.getShadowMap().renderList.push(tile);
+        this.tiles.push(tile);
+    }
+
+    private getMaterial() {
         let material = new StandardMaterial("plane", this.scene);
         material.diffuseColor = new Color3(0.7, 0.7, 0.7);
         material.specularColor = new Color3(0.7, 0.7, 0.7);
         material.ambientColor = new Color3(0, 0, 0);
         return material;
+    }
+
+    private getGroundPosition() {
+        let pickInfo = this.scene.pick(this.scene.pointerX, this.scene.pointerY, (mesh) => {
+            return mesh == this.plane;
+        });
+
+        if (pickInfo.hit) {
+            let position = pickInfo.pickedPoint;
+            position.x = Math.round(position.x);
+            position.y = Math.round(position.y);
+            position.z = 0;
+            return position;
+        }
+
+        return null;
     }
 
     private onPointerDown(event: PointerEvent) {
@@ -119,6 +215,7 @@ export class CanalBuilder {
         if (pickInfo.hit) {
             this.startingPoint = this.getGroundPosition();
             this.currentMesh = pickInfo.pickedMesh;
+            this.currentMesh.position.z = -0.5;
             if (this.startingPoint) {
                 setTimeout(() => {
                     this.camera.detachControl(this.canvas);
@@ -130,6 +227,7 @@ export class CanalBuilder {
     private onPointerUp(event: PointerEvent) {
         if (this.startingPoint) {
             this.camera.attachControl(this.canvas, true);
+            this.currentMesh.position.z = 0;
             this.currentMesh = null;
             this.startingPoint = null;
         }
@@ -146,19 +244,7 @@ export class CanalBuilder {
         let newPosition = current.subtract(this.startingPoint);
 
         this.currentMesh.position.addInPlace(newPosition);
+        this.currentMesh.position.z = -0.5;
         this.startingPoint = current;
     }
-
-    private getGroundPosition() {
-        let pickInfo = this.scene.pick(this.scene.pointerX, this.scene.pointerY, (mesh) => {
-            return mesh == this.plane;
-        });
-
-        if (pickInfo.hit) {
-            return pickInfo.pickedPoint;
-        }
-
-        return null;
-    }
-
 }
